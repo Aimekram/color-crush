f5afc8f5652c67e63511f38b03809da73669f15d
Aimekram <janina.byszewska@gmail.com>
2020-10-26 21:11:42 +0100

    Delete store initialState action - initialState is set in reducer

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,3 @@
-export const storeInitialState = data => ({
-    type: "STATE_STORED",
-    payload: { data }
-});
-
 export const changeBlocks = toDelete => ({
     type: "CLICK_HANDLED",
     payload: { toDelete }
