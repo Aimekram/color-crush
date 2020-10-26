cbb230511d8330aa36b37f7bfe590d7cf62d3a87
Aimekram <janina.byszewska@gmail.com>
2020-10-27 00:15:05 +0100

    Add some styles

--- src/styledComponents/styled.js
+++ src/styledComponents/styled.js
@@ -4,7 +4,8 @@ import {ROWS_COUNT, COLUMNS_COUNT} from "../constants";
 export const StyledBoard = styled.ul`
   width: ${COLUMNS_COUNT*100}px;
   height: ${ROWS_COUNT*100}px;
-  outline: 2px solid black;
+  box-shadow: 8px 8px 0 4px black;
+  border-radius: 8px;
   margin: 50px auto;
   display: flex;
   flex-flow: column wrap;
@@ -14,7 +15,8 @@ export const StyledBoard = styled.ul`
 export const StyledItem = styled.li`
   width: 100px;
   height: 100px;
-  border: 2px solid black;
+  border: 6px solid black;
+  border-radius: 10px;
   cursor: pointer;
   background-color: ${props => props.color};
   background-blend-mode: multiply;
